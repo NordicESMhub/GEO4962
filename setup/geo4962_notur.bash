9eb351fe418fbf874973b97af571e88045b9735c
Anne Fouilloux <annefou@geo.uio.no>
2019-02-04 10:10:17 +0100

    script to initialize cesm1.2.2 on abel

---
+++ setup/geo4962_notur.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# create cesm directory
+
+module load cesm/1.2.2
+
+if [ ! -d "$HOME/cesm" ]; then
+  mkdir $HOME/cesm
+  fi
+
+  cd $HOME/cesm
+
+  rsync -avz ${USER}@abel.uio.no:$CESM_HOME/cesm1_2_2 .
+
+  cd $HOME/cesm/cesm1_2_2/scripts 
+
+  if [ ! -d "/work/users/$LOGNAME/inputdata" ]; then
+    ./link_dirtree $CESM_HOME/inputdata /work/users/$LOGNAME/inputdata
+  fi
+
+  if [ ! -f "~/.ccsm_proj" ]; then
+cat > ~/.ccsm_proj << EOFM
+nn1000k
+EOFM
+  fi
